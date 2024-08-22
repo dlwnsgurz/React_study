cf46717bf9c024ac64b90527e28a17126c321008
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2024-08-22 14:34:57 +0900

    refs

--- refs/src/components/ResultModal.jsx
+++ refs/src/components/ResultModal.jsx
@@ -1,16 +1,29 @@
-import {forwardRef} from "react";
+import {forwardRef, useImperativeHandle, useRef} from "react";
 
-const ResultModal = forwardRef(function({result, targetTime}, ref){
+const ResultModal = forwardRef(function({targetTime, remainingTime, onReset}, ref){
+
+    const dialog = useRef();
+    const userLost = remainingTime <= 0;
+    const formattedRemaningTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+    useImperativeHandle(ref, () => {
+        return {
+            open(){
+                dialog.current.showModal();
+            }
+        }
+    });
+
     return (
-        <dialog ref={ref} className = "result-modal">
-            <h2>You {result}</h2>
+        <dialog ref={dialog} className = "result-modal">
+            {userLost ? <h2>You LOST</h2> : <h2>YOUR SCORE : {score}</h2>}
             <p>
                 The Target Time Was <strong>{targetTime} seconds.</strong> 
             </p>
             <p>
-                You stopped the timer with <strong>X seconds left.</strong>
+                You stopped the timer with <strong>{formattedRemaningTime} seconds left.</strong>
             </p>
-            <form method ="dialog">
+            <form method ="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
